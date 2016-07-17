34b8baa9e7813c343e15646cc901dc91121dc654
eduardo305 <eduardo305@gmail.com>
2016-07-17 16:52:02 -0300

    Adding Compass

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <Link to="/signout" key="1">sair</Link>
+      );
+    } else {
+      return [
+        <Link to="/signin" key="2">entrar</Link>
+      ];
+    }
+  }
 
   render() {
     return (
       <header>
         <div className="pl-header-wrapper">
+          <Link to="/" className="pl-logo-link"><img src="" alt="logo" className="pl-logo-image"></img></Link>
           <nav className="pl-navbar">
             <ul>
               <li>Quem Somos</li>
               <li>Panorama Matinal</li>
             </ul>
           </nav>
-          <Link to="/signin">Entrar</Link>
-          <Link to="/signup">Cadastrar</Link>
+          <div className="pl-auth-wrapper">
+            { this.renderLinks() }
+          </div>
         </div>
       </header>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  };
+}
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -10,11 +10,19 @@ import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={ store }>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <Route path="signin" component={Signin} />
